4c489b648012c6dedf32310d1d022ba1ec78ec33
Zach <zachhutz0@gmail.com>
2023-04-28 15:46:49 -0400

    added more validation

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from functools import wraps
 import uuid
 import jwt
 import datetime
+import re
 
 def require_token(function):
     @wraps(function)
@@ -44,82 +45,112 @@ def require_refresh_token(function):
 @app.route('/user')
 @require_token
 def get_users(current_user):
-    if not current_user.admin:
-        return jsonify({'message': "Insufficient permissions."}), 401
-    users = User.query.all()
-    if not users:
-        return jsonify({'message': 'No users found.'}), 404
-    user_list = []
-    for user in users:
-        data = {}
-        data['public_id'] = user.public_id
-        data['username'] = user.username
-        data['password'] = user.password
-        data['balance'] = user.balance
-        data['admin'] = user.admin
-        user_list.append(data)
-    return jsonify({'users: ': user_list})
+    try:
+        if not current_user.admin:
+            return jsonify({'message': "Insufficient permissions."}), 401
+        users = User.query.all()
+        if not users:
+            return jsonify({'message': 'No users found.'}), 404
+        user_list = []
+        for user in users:
+            data = {}
+            data['public_id'] = user.public_id
+            data['username'] = user.username
+            data['password'] = user.password
+            data['balance'] = user.balance
+            data['admin'] = user.admin
+            user_list.append(data)
+        return jsonify({'users: ': user_list})
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/user/<public_id>')
 @require_token
 def get_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message': "Insufficient permissions."}), 401
-    user = User.query.filter_by(public_id=public_id).first()
-    if not user:
-        return jsonify({'message': 'User not found.'})
-    data = {}
-    data['public_id'] = user.public_id
-    data['username'] = user.username 
-    data['password'] = user.password 
-    data['balance'] = user.balance
-    data['admin'] = user.admin 
-    return jsonify(data)
+    try:
+        if not current_user.admin:
+            return jsonify({'message': "Insufficient permissions."}), 401
+        user = User.query.filter_by(public_id=public_id).first()
+        if not user:
+            return jsonify({'message': 'User not found.'})
+        data = {}
+        data['public_id'] = user.public_id
+        data['username'] = user.username 
+        data['password'] = user.password 
+        data['balance'] = user.balance
+        data['admin'] = user.admin 
+        return jsonify(data)
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/user', methods=['POST'])
 @require_token
 def create_user(current_user):
-    if not current_user.admin:
-        return jsonify({'message': "Insufficient permissions."}), 401
-    data = request.get_json()
-    hashed_pw = generate_password_hash(data['password'], method='sha256')
-    user = User(public_id=str(uuid.uuid4()), username=data['username'], password=hashed_pw, admin=data['admin'])
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({'message': 'User created.'})
+    try:
+        if not current_user.admin:
+            return jsonify({'message': "Insufficient permissions."}), 401
+        data = request.get_json()
+        hashed_pw = generate_password_hash(data['password'], method='sha256')
+        user = User(public_id=str(uuid.uuid4()), username=data['username'], password=hashed_pw, admin=data['admin'])
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'User created.'})
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/user/<public_id>', methods=['DELETE'])
 @require_token
 def delete_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message': "Insufficient permissions."}), 401
-    user = User.query.filter_by(public_id=public_id).first()
-    if not user:
-        return jsonify({'message': 'User not found.'})
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'message': 'User deleted.'})
+    try:
+        if not current_user.admin:
+            return jsonify({'message': "Insufficient permissions."}), 401
+        user = User.query.filter_by(public_id=public_id).first()
+        if not user:
+            return jsonify({'message': 'User not found.'})
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted.'})
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/login')
 def login():
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return make_response(jsonify('Invalid or missing credentials.'), 401)
-    user = User.query.filter_by(username=auth.username).first()
-    if not user:
-        return make_response(jsonify('User not found.'), 404)
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'], algorithm='HS256')
-        refresh = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=15)}, app.config['SECRET_KEY'], algorithm='HS256')
-        return jsonify({'token': token, 'refresh': refresh})
+    try:
+        auth = request.authorization
+        if not auth or not auth.username or not auth.password:
+            return make_response(jsonify('Invalid or missing credentials.'), 401)
+        user = User.query.filter_by(username=auth.username).first()
+        if not user:
+            return make_response(jsonify('User not found.'), 404)
+        if check_password_hash(user.password, auth.password):
+            token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'], algorithm='HS256')
+            refresh = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=15)}, app.config['SECRET_KEY'], algorithm='HS256')
+            return jsonify({'token': token, 'refresh': refresh})
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
     return make_response(jsonify('Invalid or missing credentials.'), 401)
 
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.get_json()
-    hashed_pw = generate_password_hash(data['password'], method='sha256')
-    user = User(public_id=str(uuid.uuid4()), username=data['username'], email=data['email'], password=hashed_pw, balance=10000.00, admin=False)
-    db.session.add(user)
+    try:
+        data = request.get_json()
+        if not re.search(r".+@.+\..+", data['email']) or re.search(r"[%\\\?\"\'~/\$\*\{\}]", data['email']) or len(data['email']) > 128 or not data['email'].isascii():
+            return jsonify({'message': 'Invalid email.'})
+        if not data['username'] or re.search(r"[%\\\?\"\'~/\$\*\{\}]", data['username']) or len(data['username']) > 32 or not data['username'].isascii():
+            return jsonify({'message': 'Invalid username.'}) 
+        if not data['password'] or re.search(r"[%\\\?\"\'~/\$\*\{\}]", data['password']) or len(data["password"]) < 8 or len(data['password']) > 256 or not data['password'].isascii():
+            return jsonify({'message': 'Invalid password.'})
+        hashed_pw = generate_password_hash(data['password'], method='sha256')
+        user = User(public_id=str(uuid.uuid4()), username=data['username'], email=data['email'], password=hashed_pw, balance=10000.00, admin=False)
+        db.session.add(user)
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
     try:
         db.session.commit()
     except:
@@ -129,43 +160,55 @@ def register():
 @app.route('/refresh')
 @require_refresh_token
 def refresh_token(current_user):
-    refresh_token = request.headers['x-refresh-token']
-    if not refresh_token:
-        return make_response(jsonify({'message': 'Missing refresh token.'}), 401)
     try:
-        jwt.decode(refresh_token, app.config['SECRET_KEY'], algorithms=['HS256'])
-    except:
-        return make_response(jsonify({'message': 'Invalid refresh token.'}), 401)
-    if not current_user:
-        return make_response(jsonify({'message': 'User not found.'}), 404)
-    return jsonify({'token': jwt.encode({'public_id': current_user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'], algorithm='HS256'), 'refresh': jwt.encode({'public_id': current_user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=15)}, app.config['SECRET_KEY'], algorithm='HS256')})
+        refresh_token = request.headers['x-refresh-token']
+        if not refresh_token:
+            return make_response(jsonify({'message': 'Missing refresh token.'}), 401)
+        try:
+            jwt.decode(refresh_token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        except:
+            return make_response(jsonify({'message': 'Invalid refresh token.'}), 401)
+        if not current_user:
+            return make_response(jsonify({'message': 'User not found.'}), 404)
+        return jsonify({'token': jwt.encode({'public_id': current_user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'], algorithm='HS256'), 'refresh': jwt.encode({'public_id': current_user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=15)}, app.config['SECRET_KEY'], algorithm='HS256')})
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/stock/<symbol>', methods=['POST'])
 def get_stock_data(symbol):
-    ticker = yf.Ticker(symbol.upper())
-    data = request.get_json()
-    interval = data['interval']
-    period = data['period']
     try:
-        hist_data = get_hist_data(ticker, interval, period)
-    except:
-        return jsonify({'message': 'Invalid input.'}), 403
-    return jsonify(hist_data)
+        ticker = yf.Ticker(symbol.upper())
+        data = request.get_json()
+        interval = data['interval']
+        period = data['period']
+        try:
+            hist_data = get_hist_data(ticker, interval, period)
+        except:
+            return jsonify({'message': 'Invalid input.'}), 403
+        return jsonify(hist_data)
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/stock/get')
 @require_token 
 def get_user_stocks(current_user):
-    stocks = Stock.query.filter_by(user_id=User.query.filter_by(public_id=current_user.public_id).first().id).all()
-    if not stocks:
-        return jsonify({'message': 'User has no stocks!'})
-    stock_list = []
-    for stock in stocks:
-        data = {}
-        data['symbol'] = stock.symbol
-        data['price_at_purchase'] = stock.price_at_purchase
-        data['quantity'] = stock.quantity
-        stock_list.append(data)
-    return jsonify(stock_list)
+    try:
+        stocks = Stock.query.filter_by(user_id=User.query.filter_by(public_id=current_user.public_id).first().id).all()
+        if not stocks:
+            return jsonify({'message': 'User has no stocks!'})
+        stock_list = []
+        for stock in stocks:
+            data = {}
+            data['symbol'] = stock.symbol
+            data['price_at_purchase'] = stock.price_at_purchase
+            data['quantity'] = stock.quantity
+            stock_list.append(data)
+        return jsonify(stock_list)
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
 
 @app.route('/stock/add/<symbol>', methods=['POST'])
 @require_token
@@ -221,11 +264,15 @@ def sell_stock(current_user, symbol, quantity):
 @app.route('/stock/reset')
 @require_token 
 def reset_self(current_user):
-    stocks = Stock.query.filter_by(user_id=User.query.filter_by(public_id=current_user.public_id).first().id).all()
-    if not stocks:
-        return jsonify({'message': 'User has no stocks!'})
-    current_user.balance = 10000.00
-    for stock in stocks:
-        db.session.delete(stock)
-    db.session.commit()
-    return jsonify({'message': 'Successfully reset your stock purchases and account balance.'})
+    try:
+        stocks = Stock.query.filter_by(user_id=User.query.filter_by(public_id=current_user.public_id).first().id).all()
+        if not stocks:
+            return jsonify({'message': 'User has no stocks!'})
+        current_user.balance = 10000.00
+        for stock in stocks:
+            db.session.delete(stock)
+        db.session.commit()
+        return jsonify({'message': 'Successfully reset your stock purchases and account balance.'})
+    except Exception as e:
+        print(e)
+        return jsonify({'message': "An error occurred."}), 500
