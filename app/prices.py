ba29b058e598851176297efdf2a24aa632375618
Zach <zachhutz0@gmail.com>
2023-09-24 23:43:49 -0400

    added datetime list to returned stock data

--- app/prices.py
+++ app/prices.py
@@ -2,8 +2,9 @@ from yfinance import Ticker
 import yfinance as yf
 # interval can be 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
 # period is more flexible
-def get_hist_data(ticker: Ticker, interval: str, period: str) -> list[dict]:
-    keys = ['Open', 'Close', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits']
+def get_hist_data(ticker: Ticker, interval: str, period: str) -> dict:
+    keys = ['Datetime', 'Open', 'Close', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits']
     hist = ticker.history(interval=interval, period=period)
-    dict_list = {key: list(hist[key]) for key in keys}
+    datetime_list = list(hist.index.strftime('%Y-%m-%d %H:%M:%S'))
+    dict_list = {key: list(hist[key]) if key != 'Datetime' else datetime_list for key in keys}
     return dict_list
